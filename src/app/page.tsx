890b2f0aabaa2d8bb380b6f77092c7af3837238c
Sumbul Naz <164656538+sumbulNaz8@users.noreply.github.com>
2024-12-02 19:50:20 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     
             <main>
-              <State />
-              {/* <ParentComponent /> */}
+{/*               <State /> */}
+               <ParentComponent /> 
             </main>
        
         
